75144c836422018389527bd5fdf27e6f2e759135
David Gesl <davidgesl123@gmail.com>
2025-09-11 01:31:53 -0700

    fix: add proper syntax to config to load tailwind

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Import tailwind css plugin 
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss() // Add tailwind css plugin
+    tailwindcss(), // Add tailwind css plugin
   ],
 })
