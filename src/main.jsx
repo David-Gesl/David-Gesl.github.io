056b2390ea96fe2ea22b68b07c87befc4e0e6221
David Gesl <davidgesl123@gmail.com>
2025-09-11 01:32:40 -0700

    refactor: clean comments

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-// Components
+// css 
 import './index.css'
+
+// Components
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
