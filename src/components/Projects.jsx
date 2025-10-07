9cd093bcfba8f9ff0c7a6b11d6b977799530df86
David Gesl <davidgesl123@gmail.com>
2025-10-07 09:36:18 -0700

    fix: put projects card grid inside of container to fix spacing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,21 +41,20 @@ const Projects = () => {
       <div className="container">
 
         <h2 className="headline-1 mb-8">My Portfolio Highlights</h2>
+
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+          {works.map(({imgSrc, title, tags, projectLink}, key) => (
+            <ProjectCard 
+              key={key}
+              imgSrc={imgSrc}
+              title={title}
+              tags={tags}
+              projectLink={projectLink}
+            />
+          ))}
+        </div>
+
       </div>
-
-      <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
-        {works.map(({imgSrc, title, tags, projectLink}, key) => (
-          <ProjectCard 
-            key={key}
-            imgSrc={imgSrc}
-            title={title}
-            tags={tags}
-            projectLink={projectLink}
-          />
-        ))}
-
-      </div>
-
     </section>
   )
 }
