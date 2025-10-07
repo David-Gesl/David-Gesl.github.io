1350c9fca90fdbc354e5c7e086e9f2885dc0ecc8
David Gesl <davidgesl123@gmail.com>
2025-10-06 23:46:31 -0700

    refactor: remove redundant resume download

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,6 @@ import { useState } from "react"
 
 // Components
 import Navbar from "./Navbar"
-// Import resume asset (Vite will bundle and return a URL)
-import resumePdf from "../assets/David Gesl Resume.pdf"
 
 const Header = () => {
     const [navOpen, setNavOpen] = useState(false);
@@ -16,7 +14,6 @@ return (
             
             <div className="max-w-screen-2xl w-full mx-auto px-4 
             flex justify-between items-center md:px-6 md:grid-cols-[1fr,3fr,1fr]">
-                <h1>Hello world</h1>
 
                 <div className="relative md:justify-self-center">
                     <button className="menu-btn md:hidden" 
@@ -30,18 +27,6 @@ return (
 
                     <Navbar navOpen={navOpen}/>
                 </div>
-
-                {/* Resume button */}
-                <a
-                   href={resumePdf}
-                   target="_blank"
-                   rel="noreferrer"
-                   className="btn btn-secondary max-md:hidden md:justify-self-end"
-                >
-                  Resume
-                  <span className="material-symbols-rounded" aria-hidden="true">open_in_new</span>
-                </a>
-
             </div>
         </header>
     )
