a0c2d287835bb67ecc50796d5a13390602f0a058
David Gesl <davidgesl123@gmail.com>
2025-10-07 10:02:12 -0700

    fix: remove max width constraint

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ function About() {
     <section id="about" className='section'>
         <div className='container'>
             <div className='bg-emerald-50 p-7 rounded-2xl md:p-12 ring-1 ring-emerald-200'>
-                <p className='text-zinc-700 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]'>
-                    I'm David! I'm a Computer science and Economics double major at Oregon State University. Some of my interests are networking, real estate, economics and general problem solving. 
+                <p className='text-zinc-700 mb-4 md:mb-8 md:text-xl'>
+                    I'm David! I'm a Computer science and Economics double major at Oregon State University. Some of my interests are networking, real estate, economics and general problem solving.
                 </p>
             </div>
         </div>
