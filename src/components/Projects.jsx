165d9d5dbd3bc8bebbd33db19b3892a31c1d8bd1
David Gesl <davidgesl123@gmail.com>
2025-10-06 21:45:18 -0700

    refactor: change styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -40,7 +40,7 @@ const Projects = () => {
     <section id="projects" className="section">
       <div className="container">
 
-        <h2 className="headline-2 mb-8">My Portfolio Highlights</h2>
+        <h2 className="headline-1 mb-8">My Portfolio Highlights</h2>
       </div>
 
       <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
