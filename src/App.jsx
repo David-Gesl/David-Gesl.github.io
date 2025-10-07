a1200edcef0dd55b666a0377f590297b344be937
David Gesl <davidgesl123@gmail.com>
2025-10-06 22:43:23 -0700

    refactor: remove redundant import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 // Componenets
-import LottiePlayer from './components/LottiePlayer.jsx'       //TODO Remove this since its already used in hero?
 import Header from "./components/Header.jsx"
 import Hero from './components/hero.jsx'
 import About from './components/About.jsx'
